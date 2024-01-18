7122427de8fb7e6719ca0e7a4681fcd233483ba3
Nyadzayo <kelvin.nyadzayo16@gmail.com>
2024-01-18 20:44:30 +0200

    adding env to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ProductService } from './products/products.service';
+import { Product } from './products/product.entity';
+import { ProductController } from './products/products.controller';
 
 @Module({
-  imports: [ProductsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [Product],
+        synchronize: true,
+      }),
+    }),
+    TypeOrmModule.forFeature([Product]),
+  ],
+  controllers: [ProductController],
+  providers: [ProductService],
 })
 export class AppModule {}
